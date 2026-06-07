044e7ffeae8a37c7309560096514daa47233add1
LOPEZ BUTA <RY36857@grupo.ypf.com>
2026-06-07 16:21:40 -0300

    fix render deploy

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
 productos = [
-    {"nombre": "Taladro", "precio": 50000, "imagen": "taladro.jpg"},
-    {"nombre": "Martillo", "precio": 8000, "imagen": "martillo.jpg"},
-    {"nombre": "Destornillador", "precio": 3000, "imagen": "destornillador.jpg"}
+    {"nombre": "Taladro", "precio": 50000},
+    {"nombre": "Martillo", "precio": 8000},
+    {"nombre": "Destornillador", "precio": 3000}
 ]
 
 @app.route("/")
 def tienda():
     return render_template("index.html", productos=productos)
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
 
